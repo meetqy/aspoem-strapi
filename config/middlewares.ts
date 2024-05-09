3df33e233d2a6224e2261e0ebd2ca2390a40911a
meetqy <meetqy@icloud.com>
2024-05-09 20:43:49 +0800

    jsonLimit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,18 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::body",
+    config: {
+      jsonLimit: "10mb",
+    },
+  },
 ];
